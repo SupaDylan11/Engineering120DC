2b95645830a99f147b86fd875d8a8dd19be8d51d
Dylan Cole <dylancole@Dylans-MacBook-Pro.local>
2022-07-04 15:30:30 +0100

    Week2 Day1 lab tasks

---
+++ Week2/Code2Test/Code2Test/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Code2Test;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int timeOfDay = 21;
+        string greeting;
+        greeting = Greeting(timeOfDay);
+        Console.WriteLine(greeting);
+    }
+
+    public static string Greeting(int time)
+    {
+        if (time >= 5 && time < 12)
+            return "Good Morning";
+        else if (time >= 12 && time <= 18)
+            return "Good Afternoon";
+        else
+            return "Good Evening";
+    }
+
+    public static string GreetingV2(int time)
+    {
+        String greeting;
+
+        if (time >= 5 && time <= 12)
+            greeting = "Good Morning";
+        else if (time >= 12 && time <= 18)
+            greeting = "Good Afternoon";
+        else
+            greeting = "Good Evening";
+
+        return greeting;
+    }
+
+    // for age classifications (not normally placed in same file)
+    public static string AvailableClassifications(int ageOfViewer)
+    {
+        if (ageOfViewer < 0)
+            return "INVALID AGE ERRORS!";
+        else if (ageOfViewer < 12)
+            return "U, PG & 12 films are available.";
+        else if (ageOfViewer < 15)
+            return "U, PG, 12 & 15 films are available.";
+        else
+            return "All films are available.";
+    }
+
+    // pg = age 8 & up (but this is guidance only so will be ignored)
+    // 12 = age 12 & up
+    // 12A = any age with ADULT
+    // 15 = age 15 & up
+    // 18 = ALL
+
+    public static string CorrectedClassifications(int ageOfViewer, bool withAdult)
+    {
+        if (ageOfViewer < 0)
+            return "Invalid age given!";
+        if (withAdult)
+        {
+            if (ageOfViewer < 15)
+                return "U, PG & 12 films are available.";
+            else if (ageOfViewer < 18)
+                return "U, PG, 12 & 15 films are available.";
+            else
+                return "All films are available.";
+        }
+        else
+        {
+            if (ageOfViewer < 12)
+                return "U & PG films are available.";
+            else if (ageOfViewer < 15)
+                return "U, PG & 12 films are available.";
+            else if (ageOfViewer < 18)
+                return "U, PG, 12 & 15 films are available.";
+            else
+                return "All films are available.";
+        }
+    }
+
+    // edited version to remove redundant lines of code (as adult supervision only affects
+    // the 12A certificate movies (which we will just write as 12, as 12A is a cinema
+    // exclusive which is EXACTLY THE SAME AS A 12 CERTIFICATE.
+    public static string AltClassification(int ageOfViewer, bool withAdult)
+    {
+        if (ageOfViewer < 0)
+            return "Invalid age given!";
+        else if (ageOfViewer < 12)
+        {
+            if (withAdult)
+                return "U, PG & 12 films are available.";
+            else
+                return "U & PG films are available.";
+        }
+        else if (ageOfViewer < 15)
+            return "U, PG & 12 films are available.";
+        else if (ageOfViewer < 18)
+            return "U, PG, 12 & 15 films are available.";
+        else
+            return "All films are available.";
+    }
+}
---
+++ Week2/Code2Test/UnitTests/UnitTest1.cs
@@ -0,0 +1,105 @@
+﻿using Code2Test;
+
+namespace UnitTests;
+
+public class Tests
+{
+    [Test]
+    public void GivenATimeOf21_Greeting_ReturnsGoodEvening()
+    {
+        Assert.That(Program.Greeting(21), Is.EqualTo("Good Evening"));
+    }
+
+    [Test]
+    public void GivenATimeOf15_Greeting_ReturnsGoodAfternoon()
+    {
+        Assert.That(Program.Greeting(15), Is.EqualTo("Good Afternoon"));
+    }
+
+    [Test]
+    public void GivenATimeOf2_Greeting_ReturnsGoodEvening()
+    {
+        Assert.That(Program.Greeting(2), Is.EqualTo("Good Evening"));
+    }
+
+    [Test]
+    public void GivenATimeOf8_Greeting_ReturnsGoodAfternoon()
+    {
+        Assert.That(Program.Greeting(8), Is.EqualTo("Good Morning"));
+    }
+
+
+    // boundry value tests
+    //[Test]
+    //public void GivenATimeOf4_Greeting_ReturnsGoodEvening()
+    //{
+    //    Assert.That(Program.Greeting(4), Is.EqualTo("Good Evening"));
+    //}
+
+    //[Test]
+    //public void GivenATimeOf5_Greeting_ReturnsGoodMorning()
+    //{
+    //    Assert.That(Program.Greeting(5), Is.EqualTo("Good Morning"));
+    //}
+    //[Test]
+
+    //public void GivenATimeOf11_Greeting_ReturnsGoodMorning()
+    //{
+    //    Assert.That(Program.Greeting(11), Is.EqualTo("Good Morning"));
+    //}
+
+    //[Test]
+    //public void GivenATimeOf12_Greeting_ReturnsGoodAfternoon()
+    //{
+    //    Assert.That(Program.Greeting(12), Is.EqualTo("Good Afternoon"));
+    //}
+
+    //[Test]
+    //public void GivenATimeOf18_Greeting_ReturnsGoodAfternoon()
+    //{
+    //    Assert.That(Program.Greeting(18), Is.EqualTo("Good Afternoon"));
+    //}
+
+    //[Test]
+    //public void GivenATimeOf19_Greeting_ReturnsGoodEvening()
+    //{
+    //    Assert.That(Program.Greeting(19), Is.EqualTo("Good Evening"));
+    //}
+
+    //refined boundary tests
+    
+    [TestCase(5)]
+    [TestCase(11)]
+    public void GivenATimeOfBetween5and11_Greeting_ReturnsGoodMorning(int time)
+    {
+        Assert.That(Program.Greeting(time), Is.EqualTo("Good Morning"));
+    }
+
+    [TestCase(12)]
+    [TestCase(18)]
+    public void GivenATimeOfBetween12and18_Greeting_ReturnsGoodAfternoon(int time)
+    {
+        Assert.That(Program.Greeting(time), Is.EqualTo("Good Afternoon"));
+    }
+
+    [TestCase(19)]
+    [TestCase(4)]
+    public void GivenATimeOfBetween19and4_Greeting_ReturnsGoodMorning(int time)
+    {
+        Assert.That(Program.Greeting(time), Is.EqualTo("Good Evening"));
+    }
+
+
+    // two pararmeters
+    [TestCase(5, "Good Morning")]
+    [TestCase(11, "Good Morning")]
+    [TestCase(12, "Good Afternoon")]
+    [TestCase(18, "Good Afternoon")]
+    [TestCase(19, "Good Evening")]
+    [TestCase(4, "Good Evening")]
+    public void GivenAtTimeAndGreeting(int time, string greeting)
+    {
+        Assert.That(Program.Greeting(time), Is.EqualTo(greeting));
+    }
+
+}
