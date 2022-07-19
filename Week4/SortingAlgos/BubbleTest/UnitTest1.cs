877946a52c5377fc93e557151b0553c443119989
Dylan Cole <dylancole@Dylans-MacBook-Pro.local>
2022-07-19 16:49:28 +0100

    bubbel sort

---
+++ Week4/SortingAlgos/BubbleTest/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using SortingAlgos;
+
+namespace BubbleTest;
+
+public class Tests
+{
+    [Category("Single Value In Array")]
+    [Test]
+    public void GivenASingleValue_Return_SingleValue()
+    {
+        var array = new int[1];
+        array[0] = 5;
+        Assert.That(BubbleSort.Sort(array), Is.EqualTo(array));
+    }
+
+    [Category("Double Values In Array")]
+    [TestCase()]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+
+
+    [Category("Bubble Sorting")]
+    [Test]
+    [Ignore("f")]
+    public void Test2()
+    {
+        Assert.Pass();
+    }
+}
--- Week4/SortingAlgos/SortingAlgos/BubbleSort.cs
+++ Week4/SortingAlgos/SortingAlgos/BubbleSort.cs
@@ -1,6 +1,26 @@
 ﻿namespace SortingAlgos;
-public class Class1
-{
 
+
+
+public class BubbleSort
+{
+    public static int[] Sort(int [] intArray)
+    {
+
+        int temp;
+        for (int j = 0; j < intArray.Length - 1; j++)
+        {
+            for (int i = 0; i < intArray.Length - 1; i++)
+            {
+                if (intArray[i] > intArray[i + 1])
+                {
+                    temp = intArray[i]; // hold the larger vakue in the temp holder
+                    intArray[i] = intArray[i + 1]; // make intArray[i] be equal to smaller value
+                    intArray[i + 1] = temp; // place the temp value into the intArray[i + 1]
+                }
+            }
+        }
+        return intArray ;
+    }
 }
 
