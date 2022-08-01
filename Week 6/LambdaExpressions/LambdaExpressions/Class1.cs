340c312988d68dc8d2598a6f68b5d91424585a4c
SupaDylan11 <91839242+SupaDylan11@users.noreply.github.com>
2022-08-01 16:57:11 +0100

    Completed Work for Lamda

---
+++ Week 6/LambdaExpressions/LambdaExpressions/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressions
+{
+    internal class Person
+    {
+        public string Name { get; private set; }
+        private int _age;
+        public int Age {
+            get { return _age; }
+            set { if (value < 0)
+                    throw new ArgumentOutOfRangeException();
+                  else
+                    _age = value;
+            } }
+        public string City { get; private set; }
+
+        public Person(string name, string city, int age)
+        {
+            Name = name;
+            City = city;
+            _age = age;
+        }
+    }
+}
---
+++ Week 6/LambdaExpressions/LambdaExpressions/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace LambdaExpressions
+{
+    /*
+     * Lambders:
+     *  Have no name,
+     *  Is declared at the place it is used
+     *  cannot be reused anywhere else
+     *  The type of parameters are inferred from context.
+     */
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int>() { 3, 7, 1, 2, 8, 3, 0, 4, 5};
+
+            int allCountAlt = 0;
+            foreach (var num in nums)
+                allCountAlt++;
+
+            int countEven = 0;
+            foreach (var num in nums)
+                if (IsEven(num))
+                    countEven++;
+
+            int allCount = nums.Count(IsEven); // returns same result as the foreach loop
+
+            int countDEven = nums.Count(delegate (int x) { return x % 2 == 0; });
+            int coundLEven = nums.Count(x => x % 2 == 0);
+
+            //using a lambda expression, in one line
+            //find the total age of people at lease 30 years old
+            var people = new List<Person>
+            {
+                new Person("Nish", "Norwich", 35),
+                new Person("Rolf", "Norwich", 75),
+                new Person("Mario", "Norwich", 15)
+            };
+
+        }
+
+        public static bool IsEven(int n)
+        {
+            return n % 2 == 0;
+        }
+    }
+}
